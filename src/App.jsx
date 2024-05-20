ea57b83dffcbf3337fa84bbd1624aea017fe874c
yeeunseo-dev <anfrhrl96@gmail.com>
2024-05-20 15:53:02 +0900

    feat:계산기실패ㅠ

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-export default function App() {
+import { useState } from "react";
+
+function calc(input) {
+  const initial = 0;
+
+  const add = () => {};
+  const subtract = () => {};
+
+  const remove = input("");
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input type="number" placeholder="숫자를 입력해주세요" value={input} />{" "}
+        만큼을 <button value={add}>더할게요</button>{" "}
+        <button value={subtract}>뺄게요</button>
+        <button onClick={remove}>초기화</button>
       </div>
       <hr />
       <div>
